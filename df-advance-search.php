c76be16462057e447809c9bb184ab676e3e2aea0
Nugrah Ramadani P <nugrah.ramadani@gmail.com>
2015-06-30 09:49:55 +0700

    Initial Realease

---
+++ df-advance-search.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Dahz DF Advance Search
+Plugin URL: http://dahztheme.com/
+Description:  Enhance default Wordpress Search Form
+Version: 1.0
+Author: Dahz
+Author URI: http://dahztheme.com/
+*/
