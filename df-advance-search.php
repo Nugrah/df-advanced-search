a4caab026f2a2febbcbdd2eb79c29206ffe39c5e
Nugrah Ramadani P <nugrah.ramadani@gmail.com>
2015-09-02 14:45:33 +0700

    Bump version 1.0

--- df-advance-search.php
+++ df-advance-search.php
@@ -10,79 +10,59 @@ Author URI: http://dahztheme.com/
 License: GPL2
 */
 
+
+// File Security Checked
 if ( ! defined('ABSPATH') ) { exit; }
 
 /**
- * Adds Foo_Widget widget.
+ *
  */
-class Dahz_advance_search extends WP_Widget {
+class Dahz_Advance_Search extends WP_Widget{
 
-	/**
-	 * Register widget with WordPress.
-	 */
-	function __construct() {
-		WP_Widget::__construct(
-			'foo_widget', // Base ID
-			__( 'Dahz Advance Search', 'dahztheme' ), // Name
-			array( 'description' => __( 'Enhance default Wordpress Search Form', 'text_domain' ), ) // Args
-		);
+	function Dahz_Advance_Search(){
+		WP_Widget::__construct(false, $name = __('DF Widget - Advance Search', 'dahztheme'));
 	}
 
-	/**
-	 * Front-end display of widget.
-	 *
-	 * @see WP_Widget::widget()
-	 *
-	 * @param array $args     Widget arguments.
-	 * @param array $instance Saved values from database.
-	 */
-	public function widget( $args, $instance ) {
-		echo $args['before_widget'];
-		if ( ! empty( $instance['title'] ) ) {
-			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ). $args['after_title'];
+	function widget($args, $instance){
+		extract($args);
+
+		$title = apply_filters('widget_title',$instance['title']);
+
+		echo $before_widget;
+
+		echo '<div>';
+
+		if ( $title ) {
+			echo $before_title . $title . $after_title;
 		}
-		echo $args['after_widget'];
+
+		echo '</div>';
+
+		echo $after_widget;
 	}
 
-	/**
-	 * Back-end widget form.
-	 *
-	 * @see WP_Widget::form()
-	 *
-	 * @param array $instance Previously saved values from database.
-	 */
-	public function form( $instance ) {
-		$title = ! empty( $instance['title'] );
-		?>
-            <p>
-                  <label for="<?php echo $this->get_field_id('title') ?>">
-                  <?php echo __('Title: '); ?>
-                  <input class="widefat"/>
-                  </label>
-            </p>
-		<?php
-	}
+	function update($new_instance, $old_instance){
+		$instance = $old_instance;
 
-	/**
-	 * Sanitize widget form values as they are saved.
-	 *
-	 * @see WP_Widget::update()
-	 *
-	 * @param array $new_instance Values just sent to be saved.
-	 * @param array $old_instance Previously saved values from database.
-	 *
-	 * @return array Updated safe values to be saved.
-	 */
-	public function update( $new_instance, $old_instance ) {
-		$instance = array();
-		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
-
+		// Field
+		$instance['title'] = strip_tags($new_instance['title']);
+		$instance['text'] = strip_tags($new_instance['text']);
 		return $instance;
 	}
 
-} // class Dahz_advance_search
-// register Dahz_advance_search widget
-function register_foo_widget() {
-    register_widget( 'Dahz_advance_search' );
+	function form($instance){
+		if ($instance) {
+			$title = esc_attr($instance['title']);
+		} else {
+			$title = '';
+		}
+
+		?>
+			<p>
+				<label for="<?php echo $this->get_field_id('title') ?>"><?php _e('Title :', 'dahztheme'); ?></label>
+				<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" type="text" name="<?php echo $this->get_field_name('title') ?>" value="<?php echo $title; ?>">
+			</p>
+		<?php
+	}
 }
-add_action( 'widgets_init', 'register_foo_widget' );
+add_action('widgets_init', create_function('', 'return register_widget("Dahz_Advance_Search");'), 1);
